4ab35848dc6c88fff645963fc9fee0b10aecbc78
david.betancur <david.betancur@pragma.com.co>
2018-12-19 17:46:43 -0500

    Progress bar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,27 +4,32 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+import {FormsModule} from '@angular/forms';
 
 // rutas hijas
 import { PAGES_ROUTES } from './pages.routes';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 
 
 
 @NgModule({
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ],
     exports: [
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent
     ],
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent
     ],
     providers: [],
 })
