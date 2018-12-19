81de311da93d71ecbb959ad75560c3e56a70fcd0
david.betancur <david.betancur@pragma.com.co>
2018-12-19 11:25:38 -0500

    primer commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['../../assets/scss/pages/login-register-lock.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
