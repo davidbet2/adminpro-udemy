9a7f4c46be5548b3808761a11195d515b694ca1a
david.betancur <david.betancur@pragma.com.co>
2018-12-20 09:13:55 -0500

    Input, output, viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
+
+// componentes
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
+// modulos
 import { SharedModule } from '../shared/shared.module';
 import {FormsModule} from '@angular/forms';
+
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
 
 // rutas hijas
 import { PAGES_ROUTES } from './pages.routes';
@@ -16,20 +24,23 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
     imports: [
         SharedModule,
         PAGES_ROUTES,
-        FormsModule
+        FormsModule,
+        ChartsModule
     ],
     exports: [
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-        IncrementadorComponent
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-        IncrementadorComponent
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     providers: [],
 })
